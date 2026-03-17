0c0067632957bed1bdee8cc4f16c93a34f04fa71
Jaffleman <jaffleman@gmail.com>
2026-03-17 00:25:09 +0000

    correction code : écoute sur 0.0.0.0

--- backend/app/index.js
+++ backend/app/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 3000;
     });
 
     app.use(router);
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Listening on http://localhost:${PORT}`);
     });
   } catch (error) {
