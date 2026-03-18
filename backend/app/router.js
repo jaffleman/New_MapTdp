18a0cff127688f60783cd4c0a40bd21977698880
Jaffleman <jaffleman@gmail.com>
2026-03-18 13:09:43 +0000

    correction du router

--- backend/app/router.js
+++ backend/app/router.js
@@ -4,13 +4,13 @@ const tdpController = require("./controller/tdpController");
 
 const router = express.Router();
 
-router.post("/tdp/search", tdpController.search);
-router.post("/tdp/searchBp", tdpController.searchByPosition);
-router.post("/tdp/searchRep", tdpController.searchRep);
-router.post("/tdp/create", tdpController.create);
-router.put("/tdp/update", tdpController.update);
-router.delete("/tdp/delete", tdpController.delete);
-router.get("/tdp/updateid", tdpController.updateid);
+router.post("/search", tdpController.search);
+router.post("/searchBp", tdpController.searchByPosition);
+router.post("/searchRep", tdpController.searchRep);
+router.post("/create", tdpController.create);
+router.put("/update", tdpController.update);
+router.delete("/delete", tdpController.delete);
+router.get("/updateid", tdpController.updateid);
 router.get("/healthz", tdpController.test);
 
 module.exports = router;
