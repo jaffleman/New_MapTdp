8410b61daeeaab1e31fec0d7c000c012bb667351
Jaffleman <61849708+jaffleman@users.noreply.github.com>
2026-05-15 04:57:59 +0200

    Update Sequelize configuration with connection string
    Refactor Sequelize initialization to use connection string from environment variables.

--- backend/app/sequelize.js
+++ backend/app/sequelize.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require("sequelize");
-// console.log(process.env.PG_URL);
-// Ici on crée l'objet de type sequelize que tout mes models vont utiliser pour communiquer avec la BDD
-const sequelize = new Sequelize(process.env.PG_URL, {
-  // ici on peut rajouter des options
-  // comme par exemple des elements que l'on veut ajouter à tout les models
+
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} = process.env;
+
+const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+const sequelize = new Sequelize(connectionString, {
   define: {
     createdAt: "created_at",
     updatedAt: "updated_at",
